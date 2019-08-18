d096ef6cc25adbd131818ee0fb6e0535083ed199
liberate-hk <54244512+liberate-hk@users.noreply.github.com>
2019-08-19 00:30:28 +0800

    Initial commit
    PWA for car plate

---
+++ index.js
@@ -0,0 +1,42 @@
+function onButtonClick() {
+  const text = document.getElementById("carplate").value;
+  if(!text) return;
+  document.getElementById("submit").disabled = true;
+  fetch('/carplate.json')
+    .then(response => {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(json => {
+      console.log(json);
+      const find = json.find(item => item.id === text.trim());
+      if(!find) {document.getElementById("result").innerHTML = getRandomStatus();}
+      else {document.getElementById("result").innerHTML = "凶";}
+    })
+    .catch(err => {
+      console.log('error fetching', err);
+    })
+    .finally(() => {
+      document.getElementById("submit").disabled = false;
+    });
+}
+
+function getRandomStatus() {
+  const statusArr = ["大吉", "小吉", "中吉"];
+  const index = Math.floor(Math.random() * 3);
+  return statusArr[index];
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+      .then(function (registration) {
+          console.log("Service Worker registered with scope:", 
+                       registration.scope);
+      }).catch(function (err) {
+      console.log("Service worker registration failed:", err);
+  });
+}
---
+++ service-worker.js
@@ -0,0 +1,88 @@
+const CACHE_NAME = "carplate-v1";
+const URLS_CACHE_ONLY = [
+];
+
+const URLS_OVER_NETWORK_WITH_CACHE_FALLBACK = [
+  "/index.js",
+  "/index.html",
+  "/carplate.json",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(URLS_CACHE_ONLY.concat(URLS_OVER_NETWORK_WITH_CACHE_FALLBACK));
+      })
+      .catch(err => {
+        console.error(err);
+        return new Promise((resolve, reject) => {
+          reject('Error' + err);
+        });
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  const requestURL = new URL(event.request.url);
+  if (requestURL.pathname === '/') {
+    event.respondWith(getByNetworkFallingBackByCache("/index.html"));
+  }
+  else if(URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.href) 
+  || URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.pathname)) {
+    event.respondWith(getByNetworkFallingBackByCache(event.request))
+  }
+  else if(URLS_CACHE_ONLY.includes(requestURL.href || URLS_CACHE_ONLY.includes(requestURL.pathname))) {
+    event.respondWith(getByCacheOnly(event.request));
+  }
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+      caches.keys().then(function (cacheNames) {
+          return Promise.all(
+              cacheNames.map(function (cacheName) {
+                  if (CACHE_NAME !== cacheName && cacheName.startsWith("carplate")) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
+      })
+  );
+});
+
+/**
+ * 1. We fetch the request over the network
+ * 2. If successful we add the new response to the cache
+ * 3. If failed we return the result from the cache
+ *
+ * @param request
+ * @param showAlert
+ * @returns Promise
+ */
+const getByNetworkFallingBackByCache = (request, showAlert = false) => {
+  return caches.open(CACHE_NAME).then((cache) => {
+      return fetch(request).then((networkResponse) => {
+          cache.put(request, networkResponse.clone());
+          return networkResponse;
+      }).catch(() => {
+          if (showAlert) {
+              alert('You are in offline mode. The data may be outdated.')
+          }
+          return caches.match(request);
+      });
+  });
+};
+/**
+* Get from cache
+*
+* @param request
+* @returns Promise
+*/
+const getByCacheOnly = (request) => {
+  return caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(request).then((response) => {
+          return response;
+      });
+  });
+};
